c0c2804fdc0de367a3028585cdc0e85f703b43dd
Claes Hallström <hallstrom.claes@gmail.com>
2023-08-16 19:55:11 +0200

    Add check registry hook

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# docker-compose-registry-check
-Ensure that docker compose files only use allowed registries
+# Docker Compose Check
+
+A set of pre-commit hooks to check docker compose files.
+
+First, ensure you have `pre-commit` installed. Follow the
+[official installation guide](https://pre-commit.com/#install) for instructions.
+
+## Registry check
+
+A pre-commit hook that ensures all images in docker compose files have a defined
+registry. Valid filenames are `docker-compose.yaml` and `compose.yaml`, including
+the shorter extension `.yml`. Also supports jinja2 templates, i.e. a second extension
+`.j2` after the previously mentioned filenames.
+
+### Installation
+
+Create a `.pre-commit-config.yaml` in your repository (if it doesn't already exist)
+and add this repository to it:
+
+```yaml
+repos:
+-   repo: https://github.com/claha/docker-compose-check
+    rev: v0.1.0  # Use the ref you want
+    hooks:
+    -   id: check-registry
+        args: ['--allowed_registries=docker.io,quay.io,my.custom.registry']  # Optional
+```
---
+++ check_registry.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+"""Checking docker compose files to ensure that all images have a defined registry.
+
+Users can customize the list of allowed registries.
+"""
+import argparse
+import sys
+
+DEFAULT_ALLOWED_REGISTRIES = [
+    "docker.io",
+    "ghcr.io",
+    "lscr.io",
+    "quay.io",
+]
+
+
+def check_registry(line, allowed_registries):
+    """Check if the given line from a Docker Compose file contains a valid image registry."""
+    if "image:" not in line:
+        return True
+    image = line.split("image:")[1].strip()
+    if "/" not in image:
+        return False
+    registry = image.split("/")[0]
+    return registry in allowed_registries
+
+
+def main():
+    """Parse arguments, read and validate Docker Compose files based on the provided or default registry order."""
+    parser = argparse.ArgumentParser(
+        description="Check Docker Compose for registry in image declarations."
+    )
+    parser.add_argument("files", nargs="+", help="Docker Compose files to check.")
+    parser.add_argument(
+        "--allowed_registries",
+        default=DEFAULT_ALLOWED_REGISTRIES,
+        type=lambda s: s.split(","),
+        help="Comma-separated list of allowed image registries.",
+    )
+
+    args = parser.parse_args()
+
+    for docker_compose_file in args.files:
+        with open(docker_compose_file, "r") as file:
+            for line in file.readlines():
+                if not check_registry(line, args.allowed_registries):
+                    print(
+                        f"Invalid registry for '{line.strip()}' in {docker_compose_file}"
+                    )
+                    sys.exit(1)
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
